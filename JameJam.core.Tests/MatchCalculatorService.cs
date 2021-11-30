eb764974152dec073ffb452c34c7c3efefcb3c89
Pooyan Yazdi <p.yazdi@Innospection.com>
2021-11-30 17:33:27 +0100

    Added calculating a match factor

---
+++ JameJam.core.Tests/MatchCalculatorService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace JameJam.Binance.Core.Tests;
+
+public class MatchCalculatorService
+{
+  public double GetDifferenceFactor( List<KlinesItem> givenData, List<KlinesItem> currentRange, int historyIndex, double offset )
+  {
+    var index = historyIndex;
+    var sumOfDifferences = 0.0;
+    foreach (var currentItem in currentRange)
+    {
+      if (index < 0)
+      {
+        throw new IndexOutOfRangeException($"There is not enough elements after the {historyIndex} item in the history data to calculate a rage of {currentRange.Count} data");
+      }
+
+      var historyItem = givenData[index];
+      sumOfDifferences += Math.Abs(currentItem.High - offset - historyItem.High);
+      sumOfDifferences += Math.Abs(currentItem.Low - offset - historyItem.Low);
+      index--;
+    }
+
+    return sumOfDifferences;
+  }
+}
---
+++ JameJam.core.Tests/TestMatchCalculatorService.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace JameJam.Binance.Core.Tests;
+
+[TestFixture]
+public class TestMatchCalculatorService
+{
+  [Test]
+  public void GivenSameDataOffsetZero_WhenCalculatesMatchValue_ThenZero()
+  {
+    MatchCalculatorService service = new MatchCalculatorService();
+    var givenData = new List<KlinesItem>
+    {
+      new () {High = 1.2, Low = 1.1}, // average 1.15
+    };
+
+    var currentRange = new List<KlinesItem>()
+    {
+      new () {High = 1.2, Low = 1.1}, // average 1.15
+    };
+
+    // Action
+    var result = service.GetDifferenceFactor( givenData, currentRange, 0, 0 );
+
+    // Assert
+    result.Should().Be( 0 );
+  }
+
+  [Test]
+  public void GivenSameDataWithOffset_WhenCalculatesMatchValue_ThenZero()
+  {
+    MatchCalculatorService service = new MatchCalculatorService();
+    var givenData = new List<KlinesItem>
+    {
+      new () {High = 1.2, Low = 1.1},
+    };
+
+    var currentRange = new List<KlinesItem>()
+    {
+      new () {High = 1.2 + 5, Low = 1.1 + 5},
+    };
+
+    // Action
+    var result = service.GetDifferenceFactor( givenData, currentRange, 0, 5 );
+
+    // Assert
+    result.Should().BeApproximately( 0.0, 0.0001 );
+  }
+
+  [Test]
+  public void GivenOneDifferenceDataWithOffset_WhenCalculatesMatchValue_ThenCorrectMatchFactor()
+  {
+    MatchCalculatorService service = new MatchCalculatorService();
+    var givenData = new List<KlinesItem>
+    {
+      new () {High = 2, Low = 1},
+    };
+
+    var currentRange = new List<KlinesItem>()
+    {
+      new () {High = 2 + 5, Low = 0.5 + 5},
+    };
+
+    // Action
+    var result = service.GetDifferenceFactor( givenData, currentRange, 0, 5 );
+
+    // Assert
+    result.Should().BeApproximately( 0.5, 0.0001 );
+  }
+
+  [Test]
+  public void GivenOneDifferenceDataWithOffset1_WhenCalculatesMatchValue_ThenCorrectMatchFactor()
+  {
+    MatchCalculatorService service = new MatchCalculatorService();
+    var givenData = new List<KlinesItem>
+    {
+      new () {High = 5, Low = 3},
+    };
+
+    var currentRange = new List<KlinesItem>()
+    {
+      new () {High = 1 + 5, Low = 3 + 5},
+    };
+
+    // Action
+    var result = service.GetDifferenceFactor( givenData, currentRange, 0, 5 );
+
+    // Assert
+    result.Should().BeApproximately( 4, 0.0001 );
+  }
+
+  [Test]
+  public void GivenDifferenceDataWithOffset_WhenCalculatesMatchValue_ThenCorrectMatchFactor()
+  {
+    MatchCalculatorService service = new MatchCalculatorService();
+    var givenData = new List<KlinesItem>
+    {
+      new () {High = 5, Low = 3},
+      new () {High = 6, Low = 4},
+    };
+
+    var currentRange = new List<KlinesItem>()
+    {
+      new () {High = 5 + 5, Low = 3 + 5},
+      new () {High = 4 + 5, Low = 3 + 5},
+    };
+
+    // Action
+    var result = service.GetDifferenceFactor( givenData, currentRange, 1, 5 );
+
+    // Assert
+    result.Should().BeApproximately( 3, 0.0001 );
+  }
+}
